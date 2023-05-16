81ce95a7758453eb4e74f1081c4bdd629b6ea49d
Vijay Vishwakarma <vijay.vishwakarma@citiustech.com>
2023-05-16 18:15:39 +0530

    Initial Commit

---
+++ FirstDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace FirstDemo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://www.google.com");
+
+        driver.FindElement(By.Name("q")).SendKeys("Hello");
+
+        Thread.Sleep(10000);
+
+        driver.Quit();
+    }
+}
